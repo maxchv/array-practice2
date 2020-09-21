0e0b89e31e133491db52c01f0b365635bc1c3d95
Maxim <maxshaptala@gmail.com>
2020-09-21 13:37:26 +0300

    Выполнено перове задание. Тест проходит

--- src/main/java/ua/step/practice/Task01.java
+++ src/main/java/ua/step/practice/Task01.java
@@ -1,16 +1,26 @@
 package ua.step.practice;
 
+import java.util.Arrays;
+
 /**
  * Заполнить массив на 15 элементов числами Фибоначчи.
  * Вывести на консоль только четные элементы массива.
  * <p>
  * Пример вывода:
  * 1 2 5 13 34 89 233 610
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711
  */
 public class Task01 {
     public static void main(String[] args) {
-        // TODO: Пишите код здесь
+        int[] fib = new int[15];
+        fib[0] = fib[1] = 1;
 
+        for (int i = 2; i < fib.length; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
 
+        for (int i = 0; i < fib.length; i += 2) {
+            System.out.print(fib[i] + " ");
+        }
     }
 }
